ecec14ed14c7aed55c300c27583b854358440d8e
Fernando <fernandoza@gmail.com>
2018-10-27 19:07:43 -0500

    feat: add cookies data and bootstrap 4

--- js/main.js
+++ js/main.js
@@ -8,4 +8,11 @@
         }
     });
 
+    let cookies = new Vue({
+       el: '#cookies',
+       data: {
+           message: 'A cookie is a small text file containing information that a website transfers to your computer’s hard disk for record-keeping purposes and allows us to analyse our site traffic patterns. It does not contain chocolate chips, you cannot eat it and there is no special hidden jar.'
+       }
+    });
+
 })();
